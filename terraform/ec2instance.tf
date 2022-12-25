5ce25e29d9ad0ca96f744d45328bdeddf5e70af3
Thilagavathi P <115940590+ThilagavathiParthiban@users.noreply.github.com>
2022-12-25 15:18:01 +0530

    Create ec2instance.tf

---
+++ terraform/ec2instance.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+}
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+
+remote_state {
+  backend = "s3"
+  generate = {
+    path      = "backup.tf"
+    if_exists = "overwrite_terraform"
+  }
+  config = {
+    key = "ec2ins/instance.tfstate"
+    bucket = "terraform1-backup"
+    region = "ap-south-1"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-07ffb2f4d65357b42"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServer1"
+  }
+}

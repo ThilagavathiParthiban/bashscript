cacf586a8217dc23024d7aa5985384882d62ac66
Thilagavathi P <115940590+ThilagavathiParthiban@users.noreply.github.com>
2022-12-25 15:52:16 +0530

    Update ec2instance.tf

--- terraform/ec2instance.tf
+++ terraform/ec2instance.tf
@@ -5,30 +5,22 @@ terraform {
       version = "4.48.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform1-backup"
+    key    = "ec2"
+    region = "us-east-1"
+  }
 }
 provider "aws" {
   region  = "ap-south-1"
 }
 
 
-# remote_state {
-#   backend = "s3"
-#   generate = {
-#     path      = "backup.tf"
-#     if_exists = "overwrite_terraform"
-#   }
-#   config = {
-#     key = "ec2ins/instance.tfstate"
-#     bucket = "terraform1-backup"
-#     region = "ap-south-1"
-#   }
-# }
-
 resource "aws_instance" "app_server" {
   ami           = "ami-07ffb2f4d65357b42"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "AppServer1"
+    Name = "AppServer2"
   }
 }

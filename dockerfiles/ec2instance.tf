e8f4053c51795fb52a800a3c417b6a56d2da50fd
Thilagavathi P <115940590+ThilagavathiParthiban@users.noreply.github.com>
2022-12-26 19:08:43 +0530

    Create ec2instance.tf

---
+++ dockerfiles/ec2instance.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform1-backup"
+    key    = "ec2"
+    region = "ap-south-1"
+  }
+}
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-07ffb2f4d65357b42"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "AppServer"
+  }
+}
